83106048a319fefd4f756243304fa16f7ca89c45
okistaylor <72969270+TaylorOkis@users.noreply.github.com>
2025-05-16 09:04:56 +0100

    Fix submit route error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import formRoutes from './routes/formRoutes.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import formRoutes from "./routes/formRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -13,12 +13,12 @@ const __dirname = path.dirname(__filename);
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 
-// ejs as template engine 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// ejs as template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use('/', formRoutes);
+app.use("/", formRoutes);
 
 app.listen(port, () => {
-  console.log('server is running at http://localhost:3000')
+  console.log("server is running at http://localhost:3000");
 });
